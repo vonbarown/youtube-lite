42b8497a3c9cf7e7fbba798dce11b6e9f7b8858f
vonbarown <vonielbrown@pursuit.org>
2019-12-30 18:54:22 -0500

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-
+        <HeaderNav />
       </div>
     );
   }
--- src/containers/HeaderNav/HeaderNav.jsx
+++ src/containers/HeaderNav/HeaderNav.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Image, Menu } from 'semantic-ui-react'
+import { Image, Menu, Form, Input, Icon } from 'semantic-ui-react'
 import logo from '../../assets/images/logo.jpg'
+import './HeaderNav.css'
 
 
 class HeaderNav extends Component {
@@ -8,12 +9,41 @@ class HeaderNav extends Component {
         return (
             <Menu borderless className='top-menu' fixed='top'>
                 <Menu.Item header className='logo'>
-                    <img src={logo} size='tiny' />
+                    <Image src={logo} size='tiny' />
                 </Menu.Item>
                 <Menu.Menu className='nav-container'>
+                    <Menu.Item className='search-input'>
+                        <Form>
+                            <Form.Field>
+                                <Input placeholder='search'
+                                    size='small'
+                                    action='go'
+                                />
+                            </Form.Field>
+                        </Form>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>
+                                <Icon className='header-icon' name='video camera' size='large' />
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Icon className='header-icon' name='grid layout' size='large' />
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Icon className='header-icon' name='chat' size='large' />
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Icon className='header-icon' name='alarm' size='large' />
+                            </Menu.Item>
+                            <Menu.Item name='avatar'>
+                                <Image src='http://via.placeholder.com/80x80' avatar />
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
                 </Menu.Menu>
 
             </Menu>
         )
     }
 }
+
+export default HeaderNav
