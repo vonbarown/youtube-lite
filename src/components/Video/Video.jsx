cf8c9577dec545c1baebffb78401624e5abbb040
vonbarown <vonielbrown@pursuit.org>
2019-12-30 22:51:39 -0500

    establishing video component

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Video.scss'
+
+const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
+
+export function Video(props) {
+    if (!props.id) {
+        return null;
+    }
+    const embedUrl = `${BASE_EMBED_URL}${props.id}`;
+    return (
+        <div className='video-container'>
+            <div className='video'>
+                <iframe width={'560'} height={'315'} src={embedUrl} frameBorder='0'
+                    allow='autoplay; encrypted-media' allowFullScreen title={'video'} />
+
+            </div>
+        </div>
+    );
+}
--- src/containers/Watch/Watch.jsx
+++ src/containers/Watch/Watch.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Video } from '../../components/Video/Video'
 import './Watch.scss'
 
 export class Watch extends React.Component {
     render() {
         return (
-            <div>Watch it</div>
+            <div style={{ maxWidth: '80%' }}>
+                <Video id='-7fuHEEmEjs' />
+            </div>
         )
     }
 }
