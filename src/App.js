359493da285cc9924bb37d4adb54e79b9d45f9a1
vonbarown <vonielbrown@pursuit.org>
2019-12-30 22:24:23 -0500

    refactoring to create app layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import HeaderNav from './containers/HeaderNav/HeaderNav'
-import SideBar from './containers/SideBar/SideBar'
 import { Home } from './containers/Home/Home'
+import { AppLayout } from './components/AppLayout/AppLayout'
+
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HeaderNav />
-        <SideBar />
-        <Home />
+        <AppLayout>
+          <Home />
+        </AppLayout>
       </div>
     );
   }
