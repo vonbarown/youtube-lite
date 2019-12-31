1dfd5914d08e1e9263580c9c34420aa97739904a
vonbarown <vonielbrown@pursuit.org>
2019-12-30 22:33:12 -0500

    adding page routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import { Home } from './containers/Home/Home'
 import { AppLayout } from './components/AppLayout/AppLayout'
-
+import { Watch } from './containers/Watch/Watch'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <AppLayout>
-          <Home />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/watch' component={Watch} />
+          </Switch>
         </AppLayout>
       </div>
     );
