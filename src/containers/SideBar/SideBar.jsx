77829d0d97731461ccfd773e0217347a1d759453
vonbarown <vonielbrown@pursuit.org>
2019-12-30 20:20:09 -0500

    added footer with disclaimer

--- src/containers/SideBar/SideBar.jsx
+++ src/containers/SideBar/SideBar.jsx
@@ -3,6 +3,7 @@ import { Menu, Divider } from 'semantic-ui-react'
 import { SideBarItem } from './SideBarItem/SideBarItem'
 import { SideBarHeader } from './SideBarHeader/SideBarHeader'
 import { Subscriptions } from './Subscriptions/Subscriptions'
+import { SideBarFooter } from './SideBarFooter/SideBarFooter'
 import './SideBar.scss'
 
 
@@ -28,6 +29,7 @@ class SideBar extends Component {
                 <SideBarItem label='Help' icon='help circle' />
                 <SideBarItem label='Send feedback' icon='comment' />
                 <Divider />
+                <SideBarFooter />
             </Menu >
         )
     }
---
+++ src/containers/SideBar/SideBarFooter/SideBarFooter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SideBarFooter.scss'
+
+export const SideBarFooter = () => {
+    return (
+        <>
+            <div className='footer-block'>
+                <div>About Press Copyright</div>
+                <div>Creators Advertise</div>
+                <div>Developers +MyTube</div>
+                <div>Legal</div>
+            </div>
+            <div className='footer-block'>
+                <div>Terms Privacy</div>
+                <div>Policy & Safety</div>
+                <div>Test new features</div>
+            </div>
+            <div className='footer-block'>
+                <div>All prices include VAT</div>
+            </div>
+            <div className='footer-block'>
+                <div>A Youtube clone purely  for educational purposes under fair use.</div>
+            </div>
+        </>
+    );
+}
