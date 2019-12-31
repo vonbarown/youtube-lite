988fc07752f04ac39cd6e696e07ee69dd81f711a
vonbarown <vonielbrown@pursuit.org>
2019-12-30 21:32:54 -0500

    scaffolding home component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav'
 import SideBar from './containers/SideBar/SideBar'
-import Home from './containers/Home/Home'
+import { Home } from './containers/Home/Home'
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <HeaderNav />
         <SideBar />
+        <Home />
       </div>
-      <Home />
     );
   }
 }
