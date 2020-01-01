e07c838244463c4127a169d224cf2cfa779ad932
vonbarown <vonielbrown@pursuit.org>
2020-01-01 01:22:45 -0500

    queued video component

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { VideoPreview } from '../VideoPreview/VideoPreview';
+import { NextUpVideo } from './NextUpVideo/NextUpVideo'
 import './RelatedVideos.scss';
 
 export function RelatedVideos(props) {
     return (
         <div className='related-videos'>
+            <NextUpVideo />
             <VideoPreview horizontal={true} />
             <VideoPreview horizontal={true} />
             <VideoPreview horizontal={true} />
