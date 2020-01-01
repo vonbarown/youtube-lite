8caace6b29983e95e33039ec92bac1cec80be85b
vonbarown <vonielbrown@pursuit.org>
2020-01-01 15:17:41 -0500

    most popular video saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,16 @@
-import { all } from 'redux-saga/effects'
+import { all, call, put, fork } from 'redux-saga/effects'
+import { watchMostPopularVideos } from './video'
 export default function* () {
-    yield all([]);
+    yield all([
+        fork(watchMostPopularVideos),
+    ]);
+}
+
+export function* fetchEntity(request, entity, ...args) {
+    try {
+        const response = yield call(request);
+        yield put(entity.success(response.result, ...args));
+    } catch (error) {
+        yield put(entity.failure(error, ...args));
+    }
 }
